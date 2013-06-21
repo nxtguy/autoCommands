49ddde1779be106d6a9b7790740d097ff1c248cd
nxtguy <nxtguy@hotmail.com>
2013-06-20 18:55:45 -0700

    Added basic test code.

--- src/com/nxtcraft/Main.java
+++ src/com/nxtcraft/Main.java
@@ -3,14 +3,23 @@ package com.nxtcraft;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin {
+	
+	public static BukkitTask timer;
+	
+	public static JavaPlugin plugin;
 	
 	@Override
 	public void onEnable(){
 		PluginDescriptionFile pdfFile = getDescription();
+		plugin = this;
 		
 		Bukkit.getLogger().info(pdfFile.getName() + " v" + pdfFile.getVersion() + " has been enabled.");
+		
+		Runnable timerTask = new Timer();
+		timer = Bukkit.getServer().getScheduler().runTaskTimer(plugin, timerTask, 0L, 60L);
 	}
 	
 	@Override
---
+++ src/com/nxtcraft/Timer.java
@@ -0,0 +1,13 @@
+package com.nxtcraft;
+
+import org.bukkit.Bukkit;
+
+public class Timer implements Runnable {
+
+	@Override
+	public void run() {
+		Bukkit.getLogger().info("Timer Ran!");
+
+	}
+
+}
