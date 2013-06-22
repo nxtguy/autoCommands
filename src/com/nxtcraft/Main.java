a1acff7d11570fb73df05e7665232f7873c4fb36
nxtguy <nxtguy@hotmail.com>
2013-06-22 08:44:47 -0700

    Added configuration options, and timer features.

--- src/com/nxtcraft/Main.java
+++ src/com/nxtcraft/Main.java
@@ -1,5 +1,7 @@
 package com.nxtcraft;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,9 +10,15 @@ import org.bukkit.scheduler.BukkitTask;
 public class Main extends JavaPlugin {
 	
 	public static BukkitTask timer;
+	public static int timerID;
+	
+	public static int waitTime;
 	
 	public static JavaPlugin plugin;
 	
+	public static List<String> taskCommands;
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void onEnable(){
 		PluginDescriptionFile pdfFile = getDescription();
@@ -18,8 +26,24 @@ public class Main extends JavaPlugin {
 		
 		Bukkit.getLogger().info(pdfFile.getName() + " v" + pdfFile.getVersion() + " has been enabled.");
 		
-		Runnable timerTask = new Timer();
-		timer = Bukkit.getServer().getScheduler().runTaskTimer(plugin, timerTask, 0L, 60L);
+		plugin.getConfig().options().copyDefaults(true);
+		saveConfig();
+		
+		if (plugin.getConfig().getBoolean("Task.enable") == true) {
+			waitTime = plugin.getConfig().getInt("Task.time");
+			
+			waitTime = waitTime * 20;
+			
+			taskCommands = (List<String>) plugin.getConfig().getList("Task.commands");
+			
+			Runnable timerTask = new Timer();
+			timer = Bukkit.getServer().getScheduler().runTaskTimer(plugin, timerTask, 0L, waitTime);
+			timerID = timer.getTaskId();
+		} else {
+			Bukkit.getLogger().info("Task not enabled in the config!");
+		}
+		
+		
 	}
 	
 	@Override
@@ -28,5 +52,7 @@ public class Main extends JavaPlugin {
 		
 		Bukkit.getLogger().info(pdfFile.getName() + " v" + pdfFile.getVersion() + " has been disabled.");
 	}
+	
+	
 
 }
--- src/com/nxtcraft/Timer.java
+++ src/com/nxtcraft/Timer.java
@@ -6,7 +6,14 @@ public class Timer implements Runnable {
 
 	@Override
 	public void run() {
-		Bukkit.getLogger().info("Timer Ran!");
+		Bukkit.getLogger().info("autoCommands timer ran!");
+		
+		int x = 0;
+		while (Main.taskCommands.size() > x){
+			Main.plugin.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), Main.taskCommands.get(x));
+			
+			x++;
+		}
 
 	}
 
